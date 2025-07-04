882e8b75ac0758e4aae9c8bcf09b936aef8a2fbd
AhmetBerkeKaya <ahmetberkekaya04@gmail.com>
2025-07-04 16:22:49 +0300

    v1.1 Freelancer is deneyimi cikar ekle islemi tamamlandi

--- 4_Kaynak_Kod/Backend/tasarimcibulutu_backend/app/crud/work_experience.py
+++ 4_Kaynak_Kod/Backend/tasarimcibulutu_backend/app/crud/work_experience.py
@@ -1,6 +1,7 @@
 # app/crud/work_experience.py
 from sqlalchemy.orm import Session
 from uuid import UUID
+from app import models, schemas
 
 # --- DOĞRU IMPORT'LAR ---
 from app.models import work_experience as work_experience_model
@@ -17,4 +18,17 @@ def create_user_experience(db: Session, experience: work_experience_schema.WorkE
     db.refresh(db_experience)
     return db_experience
 
-# TODO: get, update, delete fonksiyonları buraya eklenebilir.
+# --- YENİ EKLENEN FONKSİYONLAR ---
+def update_experience(db: Session, db_experience: models.WorkExperience, experience_in: schemas.WorkExperienceUpdate) -> models.WorkExperience:
+    update_data = experience_in.dict(exclude_unset=True)
+    for key, value in update_data.items():
+        setattr(db_experience, key, value)
+    db.add(db_experience)
+    db.commit()
+    db.refresh(db_experience)
+    return db_experience
+
+def delete_experience(db: Session, db_experience: models.WorkExperience) -> models.WorkExperience:
+    db.delete(db_experience)
+    db.commit()
+    return db_experience
